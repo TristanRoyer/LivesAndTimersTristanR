84e186d3e9f5ae96a95d014ab3087f5de44cce9b
TristanRoyer <36854048+TristanRoyer@users.noreply.github.com>
2018-03-29 10:01:44 -0400

    ok

--- main.lua
+++ main.lua
@@ -25,6 +25,20 @@ local incorrectObject
 local randomOperation
 local numericField
 local score = 0
+local scoreText
+
+  
+
+
+ -- create the incorrect text object and make it invisible
+     incorrectObject = display.newText( "Incorrect", display.contentWidth/2, display.contentHeight*2/3,nil,50)
+     incorrectObject.isVisible = false
+     incorrectObject:setTextColor(100/255, 180/255, 19/255)
+
+     --create the correct text object and make it invisible
+     correctObject = display.newText( "Correct!", display.contentWidth/2, display.contentHeight*2/3,nil,50)
+     correctObject.isVisible = false
+     correctObject:setTextColor(180/255,120/255,100/255)
 ----------------------------------------------------------------------------
 --LOCAL FUNCTIONS
 ----------------------------------------------------------------------------
@@ -38,7 +52,10 @@ local function AskQuestion()
     randomNumber2 = math.random(0,21)
     randomNumber3 = math.random(0,10)
     randomNumber4 = math.random(0,10)
-    if ( randomOperation == 1)then correctAnswer = randomNumber1 + randomNumber2
+    
+    if ( randomOperation == 1) then correctAnswer = randomNumber1 + randomNumber2
+     
+     end
 
     --create question in text object
     questionObject.text = randomNumber1 .. " + " .. randomNumber2 .. " = "
@@ -46,10 +63,13 @@ local function AskQuestion()
 
     if (randomOperation == 2) then correctAnswer = randomNumber1 - randomNumber2
     --create question in text object
-    questionObject.text = randomNumber1 .. " - " .. randomNumber2.. " = "
-    elseif (correctAnswer > 0) then
+    questionObject.text = randomNumber1 .. " - " .. randomNumber2 .. " = "
+     end
+
+     if (  (correctAnswer < 0) and  (      )) then
         correctAnswer = randomNumber2 - randomNumber1
         questionObject.text = randomNumber2 .. " - " .. randomNumber1 .. " = "
+   
 end
 
 if (randomOperation == 3) then correctAnswer = randomNumber3 * randomNumber4
@@ -57,7 +77,7 @@ if (randomOperation == 3) then correctAnswer = randomNumber3 * randomNumber4
     questionObject.text = randomNumber3 .. " * " .. randomNumber4 .. " = "
 end
 
-end
+
 
 local function HideCorrect()
     correctObject.isVisible = false
@@ -96,12 +116,12 @@ local function NumericFieldListener( event )
     end
 
     local function ScoreUpdater( event )
-            score = tonumber(event.target.text)
             
             if (userAnswer == correctAnswer) then
                 score = score + 1
             end
 
+        
             
 
 end
@@ -150,10 +170,10 @@ local function UpdateTime()
         heart3.isVisible = false
         heart2.isVisible = false
         heart1.isVisible = false
+        --show image
+        gameOver.isVisible = true
 
 end
-
-
 
     	--play  sounds
         local FreeMusic = audio.loadSound("Sounds/bensound-energy.mp3")
@@ -161,12 +181,7 @@ end
         FreeMusicChannel = audio.play(FreeMusic)
 
         
-
-
-         
-        --show image
-        gameOver.isVisible = true
-       
+      
     if (lives == 4) then
     heart4.isVisible = true
     heart3.isVisible = true 
@@ -205,36 +220,24 @@ end
      questionObject = display.newText("", display.contentWidth/3, display.contentHeight/2, nil, 40)
      questionObject:setTextColor(155/255, 150/255, 100/255)
 
-     --create the correct text object and make it invisible
-     correctObject = display.newText( "Correct!", display.contentWidth/2, display.contentHeight*2/3,nil,50)
-     correctObject.isVisible = false
-     correctObject:setTextColor(180/255,120/255,100/255)
+     
 
-     -- create the incorrect text object and make it invisible
-     incorrectObject = display.newText( "Incorrect", display.contentWidth/2, display.contentHeight*2/3,nil,50)
-     incorrectObject.isVisible = false
-     incorrectObject:setTextColor(100/255, 180/255, 19/255)
+    
      -- Create Numeric field
      numericField = native.newTextField( 500, 350, 150, 30)
      numericField.inputType = "number"
 
-     -- create numeric field 2
-      numericField2 = native.newTextField( 700, 100, 180, 100)
-     numericField.inputType = "number"
-
-
+    
 
      -- add the event listener for the numeric field
      numericField:addEventListener( "userInput", NumericFieldListener )
   
     -- add the event listener for the Score Board
-     score:addEventListener( "userInput", ScoreUpdater )
-  
-   -- add the event listener for the numeric field
-     numericField:addEventListener( "userInput", NumericFieldListener )
+     numericField:addEventListener( "userInput", ScoreUpdater )
 
-      -- add the event listener for the numeric field
-     numericField:addEventListener( "userInput", NumericFieldListener )
+     numericField:addEventListener("userInput", UpdateTime)
+
+      
 
      ---------------------------------------------------------------------
      --FUNCTION CALLS
@@ -251,3 +254,11 @@ heart1.y = display.contentHeight * 1 / 7
 heart2 = display.newImageRect("images/heart.png", 100, 100)
 heart2.x =  display.contentWidth * 6 / 8
 heart2.y = display.contentHeight * 1 / 7
+
+heart3 = display.newImageRect("images/heart.png", 100, 100)
+heart3.x =  display.contentWidth * 6 / 8
+heart3.y = display.contentHeight * 1 / 7
+
+heart4 = display.newImageRect("images/heart.png", 100, 100)
+heart4.x =  display.contentWidth * 6 / 8
+heart4.y = display.contentHeight * 1 / 7
